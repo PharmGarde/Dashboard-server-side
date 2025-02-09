e53e8869f5b3633ee292e6406869dcb1b4d4e7d3
Ismail-Lafhiel <odashimidorida@gmail.com>
2025-02-09 12:09:43 +0100

    enabled cors to communicate with react

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  app.enableCors({
+    origin: process.env.VITE_FRONT_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    maxAge: 3600,
+  });
+
+  await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
 }
 bootstrap();
